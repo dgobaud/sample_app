5ca521582a845e3466a1af7278bc29d208cf6067
David Gobaud <gobaudd@gmail.com>
2012-03-05 22:14:47 +0000

    user.rb new lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,11 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
+
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
             format:     { with: valid_email_regex },
             uniqueness: { case_sensitive: false }
+
   validates :password, length: { minimum: 6}
 end
