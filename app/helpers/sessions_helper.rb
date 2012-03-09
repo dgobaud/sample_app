b1b4d500b0fbc23b3a3c445129025f5a1b9c3ead
David Gobaud <gobaudd@gmail.com>
2012-03-09 04:36:43 +0000

    Finish sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,10 @@ module SessionsHelper
     current_user = user
   end
 
+  def sign_out
+    cookies.delete(:remember_token)
+  end
+
   def signed_in?
     !current_user.nil?
   end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,15 +23,6 @@ describe "User pages" do
 
     before { visit signup_path }
 
-    describe "error messages" do
-      before { click_button "Sign up" }
-
-      let(:error) { 'errors prohibited this user from being saved' }
-
-      it { should have_selector('title', text: 'Sign up') }
-      it { should have_content(error) }
-    end
-
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button "Sign up" }.not_to change(User, :count)
@@ -59,6 +50,15 @@ describe "User pages" do
 
         it { should have_link('Sign out') }
       end
+    end
+
+    describe "error messages" do
+      before { click_button "Sign up" }
+
+      let(:error) { 'errors prohibited this user from being saved' }
+
+      it { should have_selector('title', text: 'Sign up') }
+      it { should have_content(error) }
     end
   end
 end
