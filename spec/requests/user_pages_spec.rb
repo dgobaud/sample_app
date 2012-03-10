cf0b95b3bdc9e839e43806958f07697d2f35a6a8
David Gobaud <gobaudd@gmail.com>
2012-03-10 04:57:25 +0000

    bed time

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -47,11 +47,7 @@ describe "User pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.flash.success', text: 'Welcome') }
-<<<<<<< HEAD
-=======
-
         it { should have_link('Sign out') }
->>>>>>> sign-in-out
       end
     end
 
@@ -62,6 +58,45 @@ describe "User pages" do
 
       it { should have_selector('title', text: 'Sign up') }
       it { should have_content(error) }
+    end
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Edit user") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      let(:error) { '1 error prohibited this user from being saved' }
+      before { click_button "Update" }
+
+      it { should have_content(error) }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",         with: new_name
+        fill_in "Email",        with: new_email
+        fill_in "Password",     with: user.password
+        fill_in "Confirmation", with: user.password
+        click_button "Update"
+      end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.flash.success') }
+      it { should have_link('Sign out', :href => signout_path) }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
     end
   end
 end
